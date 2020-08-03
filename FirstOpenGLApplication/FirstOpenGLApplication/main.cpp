294f8ce107fd7520def5644e71018e53d61d22db
baiwenlong <baiwenlong@58.com>
2020-08-03 20:03:12 +0800

    update glfw

---
+++ FirstOpenGLApplication/FirstOpenGLApplication/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstOpenGLApplication
+//
+//  Created by 58 on 2020/7/30.
+//  Copyright © 2020 58. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
